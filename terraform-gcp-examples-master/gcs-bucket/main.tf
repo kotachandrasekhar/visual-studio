7e604c31d226e9fd761beb80aa1affb3d7274533
KOTA CHANDRA SEKHARA  MANIKANTA <chandukota640@gmail.com>
2020-03-13 13:38:08 +0000

    second commit

--- terraform-gcp-examples-master/gcs-bucket/main.tf
+++ terraform-gcp-examples-master/gcs-bucket/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "tasdikrahman-terraform-state" {
   name          = "tasdikrahman-terraform-state"
   location      = "${var.location}"
-  force_destroy = true
+  force 
 }
